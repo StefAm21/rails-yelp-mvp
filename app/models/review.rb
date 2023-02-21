4a135e99e63484b42438ac85f064d5287334f211
stefano amodei <samodei@udesa.edu.ar>
2023-02-21 13:26:22 -0300

    ej finished

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
-  RANGE = (0...5)
+  RANGE = (0..5)
   validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: RANGE }
 end
