84ba5d97128650f381aba7800fe32621e6b045f2
stefano amodei <samodei@udesa.edu.ar>
2023-02-21 13:24:39 -0300

    ej almost finished

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  CATEGORY = %w(chinese italian japanese french belgian)
+  validates :category, inclusion: { in: CATEGORY }, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
 end
